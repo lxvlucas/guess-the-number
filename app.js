76c4d46299d3ea04beee6eb806e47e751b5d7482
Lucas Gomes Antunes <lucas04antunes43@outlook.com>
2024-11-19 15:38:17 -0300

    Mudança do limite para 100

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteadoes = [];
-let numeroLitmite = 10
+let numeroLitmite = 100
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -11,7 +11,7 @@ function exibirTextoTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoTela('h1', 'Jogo do número secreto');
-    exibirTextoTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoTela('p', 'Escolha um número entre 1 e 100');
 }
 
 exibirMensagemInicial();
