6a29b0e8a40f7392974812cd2cf764ce8e40fedb
Lucas Gomes Antunes <lucas04antunes43@outlook.com>
2024-12-02 16:52:33 -0300

    troca de 100 para 50

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteadoes = [];
-let numeroLitmite = 100
+let numeroLitmite = 50
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -11,7 +11,7 @@ function exibirTextoTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoTela('h1', 'Jogo do número secreto');
-    exibirTextoTela('p', 'Escolha um número entre 1 e 100');
+    exibirTextoTela('p', 'Escolha um número entre 1 e 50');
 }
 
 exibirMensagemInicial();
